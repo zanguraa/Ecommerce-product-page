3a7d0680e24465ee96e74cbf9243275c4e9f1e15
valeri zanguri <zanguraa@gmail.com>
2022-10-09 23:06:24 +0400

    importing tailwind.css

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Menu from "../images/icon-menu.svg";
+
+function Header() {
+  return (
+    <div>
+      <nav>
+        <img src={Menu}></img>
+        
+      </nav>
+    </div>
+  );
+}
+
+export default Header;
