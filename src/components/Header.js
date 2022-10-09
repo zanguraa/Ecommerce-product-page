ed71aac1154f80885b50ed40a8fbe61630025476
valeri zanguri <zanguraa@gmail.com>
2022-10-10 00:17:44 +0400

    navigation bar is completed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,22 @@
 import React from "react";
 import Menu from "../images/icon-menu.svg";
+import Logo from "../images/logo.svg";
+import Basket from "../images/icon-cart.svg";
+import Profile from "../images/image-avatar.png";
 
 function Header() {
   return (
     <div>
-      <nav>
-        <img src={Menu} alt="burger-menu"/>
+      <nav className="flex px-6 py-5 items-center justify-between w-full">
+        <div className="flex items-center gap-4 justify-between">
+          <img src={Menu} alt="burger-menu" />
+          <img className="h-[20px]" src={Logo} alt="logo" />
+        </div>
 
+        <div className="flex items-center gap-6 justify-between">
+          <img src={Basket} alt="cart" />
+          <img className=" h-6" src={Profile} alt="avatar-user" />
+        </div>
       </nav>
     </div>
   );
